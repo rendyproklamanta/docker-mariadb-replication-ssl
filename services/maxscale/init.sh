31538f4ef65b71cc10dc38377d1d3e4e9e6bf02d
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-11-01 10:03:58 +0700

    update

--- services/maxscale/init.sh
+++ services/maxscale/init.sh
@@ -2,3 +2,7 @@
 
 ## Replace text
 find "$BASE_DIR" -type f -exec sed -i "s|MAXCONF_PASSWORD_SET|$MAXSCALE_PASSWORD|g" {} +
+
+# MOVE : Conf
+echo -e "${YELLOW}**** Moving maxscale conf directory ****${NC}"
+mv -f $SERVICE_DIR/maxscale/conf $DATA_DIR/conf/maxscale
