86338ee04819937fdfaafe1aac873ae86418030c
Rendy Dwi Proklamanta <rendyproklamanta@gmail.com>
2024-12-11 10:14:10 +0700

    update

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -11,6 +11,8 @@ container_id=$(docker ps -q -f "name=$host")
 
 # Function to check is running inside the container
 check_mariadb_running() {
+  echo "Checking $host is running..."
+  sleep 5
   docker exec $container_id mariadb -u$user --password=$pass --execute="SELECT 1" > /dev/null 2>&1
 }
 
